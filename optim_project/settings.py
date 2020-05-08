e698d57a41bac3b833a9885ae6723d0c944f49ba
evgeny <shchepinevg@gmail.com>
2020-05-08 23:54:29 +0700

    Connected to PostgreSql and added .gitignore

--- optim_project/settings.py
+++ optim_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'optim_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'optimization',
+        'USER': 'admin',
+        'PASSWORD': 'amqPXkfLKN6U',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
